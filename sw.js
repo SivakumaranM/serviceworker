813cf442392dec81d15ce379fe5e685dbec6147d
Sivakumaran Manoharan <sivakumaran.manoharan@gmail.com>
2016-12-29 18:30:25 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'cache-v1';
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        '/sivakumaranm/',
+        '/sivakumaranm/index.html',
+        '/sivakumaranm/app.js'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open(CACHE_NAME).then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  });
+});
