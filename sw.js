f82b25937a6becf5d5a4bc79e2a6b47ad2a8eb00
Sivakumaran Manoharan <sivakumaran.manoharan@gmail.com>
2016-12-30 16:05:13 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-var CACHE_NAME = 'cache-v1';
+var CACHE = 'cache-v1';
 
 this.addEventListener('install', function(event) {
+  console.log('The service worker is being installed');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE).then(function(cache) {
       return cache.addAll([
         '/serviceworker/',
         '/serviceworker/index.html',
@@ -15,15 +16,23 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
   console.log('The service worker is serving the asset.');
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
+  event.respondWith(fromCache(event.request).catch(function() {
     console.log('Asset is not available in cache. Fetching from network');
     return fetch(event.request);
   }).then(function(r) {
     response = r;
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE).then(function(cache) {
       cache.put(event.request, response);
       console.log('Adding asset to cache');
     });
     return response.clone();
   }));
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
